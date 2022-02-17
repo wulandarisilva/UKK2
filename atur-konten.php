8bc16452b381e0d51d2d9ddcf54da302f191bf60
wulandarisilva <97926604+wulandarisilva@users.noreply.github.com>
2022-02-17 08:19:48 +0700

    Add files via upload

---
+++ atur-konten.php
@@ -0,0 +1,24 @@
+<?php
+if ($_GET['menu'] == 'home') {
+  include("konten/home.php");
+}
+elseif ($_GET['menu'] == 'logout') {
+  session_start();
+  session_destroy();
+  print "<meta http-equiv='refresh' content='0; url=?menu=login'>";
+}
+elseif ($_GET['menu'] == 'kirim-pengaduan') {
+  include('konten/kirim-pengaduan.php');
+}
+elseif ($_GET['menu'] == 'pengaduan') {
+  include('konten/pengaduan.php');
+}
+elseif ($_GET['menu'] == 'verifikasi') {
+  include('konten/verifikasi.php');
+}
+elseif ($_GET['menu'] == 'kirim-tanggapan') {
+  include('konten/kirim-tanggapan.php');
+}
+elseif($_GET['menu'] == 'about') {
+  print "<H1> INI HALAMAN ABOUT!!! </H1>";
+}
